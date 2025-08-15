e97affefeab071c794e66899530d84ec92298b5e
ibrahim demirci <ibodemirci333@gmail.com>
2025-08-15 12:22:25 +0300

    backend gun 3

--- Backend/app.js
+++ Backend/app.js
@@ -1,17 +1,24 @@
 require('dotenv').config();
-
+const config = require('./config/config');
 const express = require('express');
 const connectDB = require('./config/database');
-const config = require('./config/config');
+
 const globalErrorHandler = require('./middlewares/globalErrorHandler');
+const createHttpError = require('http-errors');
+const cookieParser = require('cookie-parser');
 const app = express();
 
 const PORT = config.PORT; 
 connectDB();
 
+app.use(express.json());
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
-  res.json({message : "Hello from server!"});
+  res.json({message : "sunucudan selamlar!"});
 });
+
+app.use("/api/user", require('./routes/userRoute'));
 
 app.use(globalErrorHandler);
 
--- Backend/config/config.js
+++ Backend/config/config.js
@@ -4,5 +4,6 @@ const config = Object.freeze({
     PORT: process.env.PORT || 3000,
     databaseURI: process.env.MONGODB_URI || "mongodb://localhost:27017/mydatabase",
     nodeEnv : process.env.NODE_ENV || "development",
+    accessTokenSecret: process.env.JWT_SECRET
 });
 module.exports = config;
